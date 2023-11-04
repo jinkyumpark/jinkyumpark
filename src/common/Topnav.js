5477db637092a3e6c95e7d12f700e478fd31af92
Jinkyum Park <jinpark1025@gmail.com>
2023-11-04 12:26:48 +0900

    change topnav label to Korean again

--- src/common/Topnav.js
+++ src/common/Topnav.js
@@ -7,18 +7,23 @@ import Navbar from 'react-bootstrap/Navbar'
 const Topnav = () => {
 	const [currentUrl] = useState(window.location.pathname)
 
+	// TODO : localized by getting language from url
+	const title = "서버개발자 박진겸"
+	const portfolio = "포트폴리오"
+	const proudct = "제품"
+
 	return (
 		<>
 			<Navbar bg='dark' variant='dark' fixed='top' className='p-1 m-0'>
 				<Container>
-					<Navbar.Brand href={`${process.env.PUBLIC_URL}/`}>JINKYUM PARK</Navbar.Brand>
+					<Navbar.Brand href={`${process.env.PUBLIC_URL}/`}>{title}</Navbar.Brand>
 					<Nav className='me-auto'>
 						<Nav.Link href={`${process.env.PUBLIC_URL}/portfolio`} active={currentUrl.startsWith('/portfolio')}>
-							Portfolios
+							{portfolio}
 						</Nav.Link>
 
 						<Nav.Link href={`${process.env.PUBLIC_URL}/product`} active={currentUrl.startsWith('/product')}>
-							Products
+							{proudct}
 						</Nav.Link>
 					</Nav>
 				</Container>
