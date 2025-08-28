fc633a13a8827a0ad6c563ba98a55eb66d5ee576
Jinkyum Park <me@jinkyumpark.com>
2025-08-29 03:17:04 +0900

    fix Footer layout for improved responsiveness

--- src/common/Footer.tsx
+++ src/common/Footer.tsx
@@ -1,53 +1,63 @@
 import React from 'react'
-import Container from 'react-bootstrap/Container'
 import styled from 'styled-components';
 import WebsiteIcon from '../data/websiteIcon';
+import breakpoints from "./breakpoints";
 
 const Footer = () => {
     return (
-        <Container className='w-100'>
-            <footer className="d-flex justify-content-between py-3 my-4">
-                <div className="col-6 col-md-4 d-flex align-items-start flex-column">
-                    <span className="text-muted">박진겸, Jinkyum Park</span>
-                    <a className="text-muted text-decoration-none"
-                       href="mailto:jinkyumpark@naver.com">jinkyumpark@naver.com</a>
-                </div>
+        <Container>
+            <div className="col-12 col-sm-6 col-md-8 p-0">
+                <div className="text-muted">박진겸, Jinkyum Park</div>
 
-                <div className="d-flex justify-content-end col-6 col-md-4">
-                    <a className="text-muted ms-1 me-1 hover-effect" href="https://booksitout.com" target='_blank'
-                       rel="noreferrer">
-                        <Image src={WebsiteIcon.booksitout} alt=''/>
-                    </a>
+                <a className="text-muted text-decoration-none"
+                   href="mailto:jinkyumpark@naver.com">jinkyumpark@naver.com</a>
+            </div>
 
-                    <a className="text-muted ms-1 me-1 hover-effect"
-                       href="https://apps.apple.com/us/app/substrack-bills-tracker/id6451234943"
-                       target='_blank' rel="noreferrer">
-                        <Image src={WebsiteIcon.substrackIcon} alt=''/>
-                    </a>
+            <div className="col-12 col-sm-6 col-md-4 p-0 pt-4 pt-md-0 text-end">
+                <a className="text-muted ms-1 me-1 hover-effect" href="https://booksitout.com" target='_blank'
+                   rel="noreferrer">
+                    <Image src={WebsiteIcon.booksitout} alt=''/>
+                </a>
 
-                    <a className="text-muted ms-1 me-1 hover-effect"
-                       href="https://apps.apple.com/app/purrfit-tracker/id6458539987" target='_blank' rel="noreferrer">
-                        <Image src={WebsiteIcon.purrfitTracker} alt=''/>
-                    </a>
+                <a className="text-muted ms-1 me-1 hover-effect"
+                   href="https://apps.apple.com/us/app/substrack-bills-tracker/id6451234943"
+                   target='_blank' rel="noreferrer">
+                    <Image src={WebsiteIcon.substrackIcon} alt=''/>
+                </a>
 
-                    <a className="text-muted ms-1 me-1 hover-effect"
-                       href="https://apps.apple.com/us/app/featured-days/id6523433258"
-                       target='_blank' rel="noreferrer">
-                        <Image src={WebsiteIcon.featuredDays} alt=''/>
-                    </a>
+                <a className="text-muted ms-1 me-1 hover-effect"
+                   href="https://apps.apple.com/app/purrfit-tracker/id6458539987" target='_blank' rel="noreferrer">
+                    <Image src={WebsiteIcon.purrfitTracker} alt=''/>
+                </a>
 
-                    <a className="text-muted ms-1 me-1 hover-effect"
-                       href="https://apps.apple.com/us/app/pianoman/id6456449788"
-                       target='_blank' rel="noreferrer">
-                        <Image src={WebsiteIcon.pianoman} alt='' className={'border'}/>
-                    </a>
-                </div>
-            </footer>
+                <a className="text-muted ms-1 me-1 hover-effect"
+                   href="https://apps.apple.com/us/app/featured-days/id6523433258"
+                   target='_blank' rel="noreferrer">
+                    <Image src={WebsiteIcon.featuredDays} alt=''/>
+                </a>
+
+                <a className="text-muted ms-1 me-1 hover-effect"
+                   href="https://apps.apple.com/us/app/pianoman/id6456449788"
+                   target='_blank' rel="noreferrer">
+                    <Image src={WebsiteIcon.pianoman} alt='' className={'border'}/>
+                </a>
+            </div>
         </Container>
     )
 }
 
 export default Footer
+
+const Container = styled.footer.attrs({
+    className: 'row'
+})`
+    padding: 50px 100px;
+    margin: 0;
+    
+    @media (max-width: ${breakpoints.md}) {
+        padding: 50px 20px;
+    }
+`
 
 const Image = styled.img.attrs({
     className: `img-fluid rounded`,
